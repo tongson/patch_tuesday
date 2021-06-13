956866aebea30ec3c0bcd4fe5845f4fa37df4182
Eduardo Tongson <propolice@gmail.com>
2021-06-13 20:11:41 +0800

    Comments for people looking.

--- main.go
+++ main.go
@@ -20,13 +20,19 @@ func main() {
 	runtime.MemProfileRate = 0
 	L := lua.NewState()
 	defer L.Close()
+	// Load `http` and `json` modules
 	L.PreloadModule("http", gluahttp.Xloader)
 	L.PreloadModule("ll_json", json.Loader)
+	// Load lua code to patch `table` and `string`; found in `LadyLua/src/lua`
 	ll.PatchLoader(L, "table")
 	ll.PatchLoader(L, "string")
+	// Load all plain Lua code from LadyLua; found in `LadyLua/src/lua`
 	ll.EmbedLoader(L)
+	// Load Lua source from `src`; for `require("cvrf")`
 	ll.ModuleLoader(L, "cvrf", ll.ReadFile(luaSrc, "src/cvrf.lua"))
+	// Capture command line arguments
 	ll.FillArg(L, os.Args)
+	// Load Lua source from `main`; the main() Lua code
 	ll.MainLoader(L, ll.ReadFile(mainSrc, "main/main.lua"))
 	os.Exit(0)
 }
