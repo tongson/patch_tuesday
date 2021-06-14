2c3472254aa2ac4a14de88554e15e5014e8d96fc
Eduardo Tongson <propolice@gmail.com>
2021-06-14 11:03:31 +0800

    Linebreaks for readability.

--- main.go
+++ main.go
@@ -20,19 +20,25 @@ func main() {
 	// Load `http` and `json` modules
 	ll.GoLoader(L, "http")
 	ll.GoLoader(L, "json")
+
 	// Load lua code to patch `table` and `string`; found in `LadyLua/src/lua`
 	ll.PatchLoader(L, "table")
 	ll.PatchLoader(L, "string")
+
 	// Allowing loading(require) Lua code from LadyLua; found in `LadyLua/src/lua`
 	ll.EmbedLoader(L)
+
 	// Load Lua source from `src`; for `require("cvrf")`
 	// Usually modules specific to a project or program
 	// Depends on the go:embed directive, any directory or filename works
 	ll.ModuleLoader(L, "cvrf", ll.ReadFile(luaSrc, "src/cvrf.lua"))
+
 	// Capture command line arguments
 	ll.FillArg(L, os.Args)
+
 	// Load Lua source from `main`; the entrypoint Lua code or so-called main()
 	// Depends on the go:embed directive, any directory or filename works
 	ll.MainLoader(L, ll.ReadFile(mainSrc, "main/main.lua"))
+
 	os.Exit(0)
 }
