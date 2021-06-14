2a45cf089ba598f17aa59013ddcc899dbcda05a7
Eduardo Tongson <propolice@gmail.com>
2021-06-14 10:45:21 +0800

    Rollback. Was testing errors.

--- src/cvrf.lua
+++ src/cvrf.lua
@@ -6,7 +6,7 @@ return {
 			timeout = "30s",
 			headers = { Accept = "application/json" },
 		}
-		local url = ("https://www.msrc.microsoft.com/cvrf/v2.0/cvrf/%s"):format(a.id)
+		local url = ("https://api.msrc.microsoft.com/cvrf/v2.0/cvrf/%s"):format(a.id)
 		local req, err = http.get(url, options)
 		if not req or not req.status == 200 then
 			return nil, err or "cvrf: Did not return a valid response."
