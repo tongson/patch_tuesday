5febaf8407799969dc59427a260f152ed82fa0ef
Eduardo Tongson <propolice@gmail.com>
2021-06-18 13:15:21 +0800

    Note where these are used.

--- main/main.lua
+++ main/main.lua
@@ -1,6 +1,6 @@
 -- 2021-Jun-12 https://github.com/Immersive-Labs-Sec/msrc-api/ in Lua
-require("xstring")()
-require("xtable")()
+require("xstring")() -- "string":contains(), "string":trim_end()
+require("xtable")() -- table.size
 local json = require("json")
 local http = require("http")
 local fmt = require("fmt")
